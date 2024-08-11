0c753264ad6a3033be1470bb7bc1f94dd1048176
Jun Gong <jun@evolutionaryscale.ai>
2024-08-11 18:30:43 +0000

    finish sagemaker client

--- esm/sdk/sagemaker.py
+++ esm/sdk/sagemaker.py
@@ -1,26 +1,34 @@
-from urllib.parse import urljoin
+import json
 
-import requests
+import boto3
 
 from esm.sdk.forge import ESM3ForgeInferenceClient
 
 
 class ESM3SageMakerClient(ESM3ForgeInferenceClient):
-    # TODO(jungong) : get rid of url parameter after we switch to SageMaker Runtime.
-    def __init__(self, model: str, endpoint_name: str, url: str):
+    def __init__(self, endpoint_name: str, model: str):
+        """ESM3 client that talks to a SageMaker endpoint.
+
+        Args:
+            endpoint_name: Name of the SageMaker endpoint.
+            model: Name of the ESM3 model.
+        """
         # Dummy URL and token to make ESM3ForgeInferenceClient happy.
-        super().__init__(model=model, url=url, token="sagemaker")
+        super().__init__(model=model, url="", token="dummy")
 
         self._endpoint_name = endpoint_name
+        self._model = model
 
-    def __post(self, endpoint, request, potential_sequence_of_concern):
+        self._client = boto3.client(service_name="sagemaker-runtime")
+
+    def _post(self, endpoint, request, potential_sequence_of_concern):
         request["potential_sequence_of_concern"] = potential_sequence_of_concern
 
         invocations_request = {
             # Duplicate these fields at the top level to make Forge requests consistent.
             "model": request["model"],
-            "request_id": request["request_id"],
-            "user_id": request["user_id"],
+            "request_id": "",  # Forge specific field.
+            "user_id": "",  # Forge specific field.
             # Invocation data bits.
             "api_ver": "v1",  # Must be v1 right now.
             "endpoint": endpoint,
@@ -28,21 +36,16 @@ class ESM3SageMakerClient(ESM3ForgeInferenceClient):
             endpoint: request,
         }
 
-        # Note: this does NOT actually work on SageMaker.
-        # This temporary implementation is for testing only for now.
-        # TODO(jungong) : switch to sagemaker_runtime.invoke_endpoint()
-        # after I figure out how to test this on SageMaker for real.
-        response = requests.post(
-            urljoin(self.url, "/invocations"),  # SageMaker's one & only entrypoint.
-            json=invocations_request,
-            headers=self.headers,
-            timeout=self.request_timeout,
-        )
+        try:
+            response = self._client.invoke_endpoint(
+                EndpointName=self._endpoint_name,
+                ContentType="application/json",
+                Body=json.dumps(invocations_request),
+            )
+        except Exception as e:
+            raise RuntimeError(f"Failure in {endpoint}: {e}")
 
-        if not response.ok:
-            raise RuntimeError(f"Failure in {endpoint}: {response.text}")
-
-        data = response.json()
+        data = json.loads(response["Body"].read().decode())
 
         # Response must match request.
         assert data["endpoint"] == endpoint
